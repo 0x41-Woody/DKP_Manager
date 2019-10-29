ce5022f1987db2ab5319783eddbaf92943962eef
0x41-Woody <0x41.woody@gmail.com>
2019-10-28 22:51:58 -0500

    api module setup

--- app.js
+++ app.js
@@ -2,4 +2,7 @@ const express = require('express');
 const app = express();
 const db = require('./db');
 
+const API = require('./api/APIController');
+app.use('/api', APIContoller);
+
 module.exports = app;
